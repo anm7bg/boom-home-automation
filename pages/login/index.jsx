50346570ac3a6eb99bd24c36ccd14ed45efd85ef
A. Maslarov <nsite2014@gmail.com>
2023-03-13 16:40:01 +0200

    Edit login form style

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,13 +1,14 @@
 import styles from "./LoginPage.module.scss";
 import { Container, Paper } from "@mui/material";
 import Login from "../../src/components/login/Login";
+import classNames from "classnames";
 
 export default function LoginPage({  }) {
 
     return(
-        <div className={styles.wrapper}>
-            <Container>
-                <Paper elevation={3} >
+        <div className={classNames(styles.wrapper)}>
+            <Container className={classNames(styles.container)} maxWidth="xl">
+                <Paper className={classNames(styles.paper)}>
                     <Login />
                 </Paper>
             </Container>
