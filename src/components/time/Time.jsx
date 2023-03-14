a757754de79adc33ac95bffd8e8a44ed93485ec5
A. Maslarov <nsite2014@gmail.com>
2023-03-14 18:45:19 +0200

    Added time component

---
+++ src/components/time/Time.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Time.module.scss";
+
+export default function Time() {
+
+    const [time, setTime] = useState("");    
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const today = new Date()
+            setTime(time => today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds());
+            return () => clearInterval(interval);
+        },1000)
+    },[])
+
+    return(
+        <div className={styles["time-wrapper"]}>
+            <p>Time</p>
+            <p>{time}</p>
+        </div>
+    );
+}
