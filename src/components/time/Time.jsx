48f4354b0c4b7cec7fae25ef6aad6767354fd5f7
A. Maslarov <nsite2014@gmail.com>
2023-03-15 16:30:50 +0200

    Test

--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -15,7 +15,6 @@ export default function Time() {
     },[])
 
     return(
-
         <div className={styles["time-wrapper"]}>
             <Typography>Time</Typography>
             <Typography>{time}</Typography>
